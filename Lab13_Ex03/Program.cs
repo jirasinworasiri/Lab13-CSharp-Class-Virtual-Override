0ba8dd97b635be80b2f3b4d4b2de9dd319d79aaa
jirasinworasiri <aomjirasin457@gmail.com>
2025-03-27 16:20:47 +0700

    66030029 จิรสิน

---
+++ Lab13_Ex03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
+
+public class AnimalSound
+{
+    public void PlaySound(Animal animal)
+    {
+        animal.MakeSound();
+    }
+}
+
+// โปรแกรมหลักสำหรับทดสอบ
+class Program
+{
+    static void Main()
+    {
+        AnimalSound soundPlayer = new AnimalSound();
+
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        soundPlayer.PlaySound(myDog); // Output: Dog barks
+        soundPlayer.PlaySound(myCat); // Output: Cat meows
+    }
+}
